8ce05ea97265007807267c88b7a5db8d80027a9c
joshc3453 <joshc3453@gmail.com>
2024-03-26 21:32:45 -0400

    Adding team rosters

--- team_rosters.py
+++ team_rosters.py
@@ -3,6 +3,7 @@ from nba_api.stats.static import teams
 from sqlalchemy import create_engine
 import time
 from datetime import datetime, timedelta
+import pandas as pd
 
 teams_list = teams.get_teams()
 
@@ -11,25 +12,16 @@ team_list_ids = [team['id'] for team in teams_list]
 # This will be used to dynamically create roster tables for each team
 ids_and_names_list = [{'id': team['id'], 'full_name': team['full_name']} for team in teams_list]
 
+connection_string = "postgresql+psycopg2://airflow:airflow@localhost/postgres"
+engine = create_engine(connection_string)
 
-for team in teams_list[:3]:
-    # CommonTeamRoster(team_id=team['id']).get_data_frames()[0]
+for team in teams_list:
     team_name = team['full_name'].lower().replace(' ', '_')
     df = CommonTeamRoster(team_id=team['id']).get_data_frames()[0]
     df.columns = [c.lower() for c in df.columns]
+    df['birth_date'] = pd.to_datetime(df['birth_date']).dt.date
     df.rename(columns={'teamid': 'team_id', 'leagueid': 'league_id'})
-    print(df.head())
+    table_name = f"{team_name}_roster_{df['season'][0]}"
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+    print(f"{table_name} loaded successfully.")
     time.sleep(0.6)
-
-## Next Steps - Convert birth_date to datetime
-
-# # Original date string to be converted
-# date_str = "APR 09, 1999"
-
-# # Convert the string to a datetime object to enable manipulation.
-# # "%b %d, %Y" specifies the format of the input string: abbreviated month, day, and full year.
-# date_obj = datetime.strptime(date_str, "%b %d, %Y")
-
-# # Format the datetime object into a string with the desired format.
-# # "%Y-%m-%d" specifies the output format: full year, month, and day.
-# formatted_date_str = date_obj.strftime("%Y-%m-%d")
